a7a34b01e703c4389d94ab957aa7aac7cd3f7eb5
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-12-08 10:27:35 +0300

    Added BreezeUIKit

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,53 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// Set project paths
+const localDomain = 'http://docean.test';
+
+mix
+  // Utilities
+  .sourceMaps()
+  .options({
+    processCssUrls: false
+  })
+
+  // Add global libraries
+  .autoload({
+    jquery: ['$', 'jQuery'],
+    uikit: 'UIkit'
+  })
+
+  // Suppress success messages
+  .disableSuccessNotifications()
+
+  // Compile Javascript (ES6)
+  .js('resources/js/app.js', 'public/js').extract()
+
+  // Compile Sass
+  .sass('resources/scss/app.scss', 'public/css')
+
+  // .copy('resources/img', 'public/img')
+  // .copy('resources/fonts', 'public/fonts')
+
+  // Setup BrowserSync
+  .browserSync({
+    proxy: localDomain,
+    host: localDomain.replace(/^https?:\/\//, ''),
+    notify: false,
+    open: false,
+    injectChanges: true,
+    // https: {
+    //   key: '/Users/YOUR_COMPUTER/.config/valet/Certificates/YOUR_SITE.test.key',
+    //   cert: '/Users/YOUR_COMPUTER/.config/valet/Certificates/YOUR_SITE.test.crt'
+    // }
+  })
+
+// Setup versioning (cache-busting)
+if (mix.inProduction()) {
+  mix.version()
+}
